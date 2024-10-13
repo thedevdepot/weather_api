ab0665d65ba4e1c80097b9d82255d25ec774e078
Python Mega Course <tonyringertx@gmail.com>
2024-10-13 16:39:54 -0500

    configured flask to begin weather api

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 from flask import Flask, render_template
 
-app = Flask("Website")
+app = Flask(__name__)
 
-@app.route("/home")
+
+@app.route("/")
 def home():
-    return render_template("tutorial.html")
+    return render_template("home.html")
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
 
-@app.route("/contact_us")
-def contact_us():
-    return render_template("contact_us.html")
+@app.route("/api/v1/<station>/<date>")
+def home(station, date):
+    df.pd.read_csv("")
+    temperatrue = df.station(date)
+    return render_template("home.html")
 
-app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(debug=True)
