3a7e887773d9857bab041a1c6d4dcaa4ac883a7f
Python Mega Course <tonyringertx@gmail.com>
2024-10-16 21:08:24 -0500

    Setting up api to return station data using pd

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -9,10 +10,13 @@ def home():
 
 
 @app.route("/api/v1/<station>/<date>")
-def home(station, date):
-    df.pd.read_csv("")
-    temperatrue = df.station(date)
-    return render_template("home.html")
+def about(station, date):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["   DATE"])
+    temperatrue = df.loc[df["    DATE"] == date]["    TG"].squeeze() / 10
+    return {"station": station,
+            "date": date,
+            "temperature": temperature}
 
 
 if __name__ == "__main__":
