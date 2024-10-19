c5ed4038062c88710067072292459f9ba80c0b6c
Python Mega Course <tonyringertx@gmail.com>
2024-10-18 22:27:01 -0500

    Added station variation of api output for full year data output

--- main.py
+++ main.py
@@ -15,11 +15,18 @@ def home():
 @app.route("/api/v1/<station>/<date>")
 def about(station, date):
     filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
-    df = pd.read_csv(filename, skiprows=20, parse_dates=["   DATE"])
-    temperatrue = df.loc[df["    DATE"] == date]["    TG"].squeeze() / 10
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    temperature = df.loc[df["    DATE"] == date]["    TG"].squeeze() / 10
     return {"station": station,
             "date": date,
             "temperature": temperature}
+
+@app.route("/api/v1/<station>")
+def all_data(station):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    result = df.to_dict(orient="records")
+    return result
 
 
 if __name__ == "__main__":
